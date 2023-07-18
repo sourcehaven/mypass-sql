a0ed9955e6190df42cb5bddf37cf6a3e5d13a34e
Ricky294 <horypatrik98@gmail.com>
2023-07-18 11:07:53 +0200

    Replacing custom solution with PLY.

--- mypass/lexer.py
+++ mypass/lexer.py
@@ -1,41 +1,111 @@
-import abc
+
 import re
 
-from mypass.tokens import Token
+from ply import lex
+
+from tokens import tokens # noqa
 
 
-def get_concrete_classes(cls):
-    concrete_classes = []
-    for subclass in cls.__subclasses__():
-        if abc.ABC not in subclass.__bases__:
-            concrete_classes.append(subclass)
-        else:
-            concrete_classes.extend(get_concrete_classes(subclass))
-    return concrete_classes
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_MULTIPLY = r'\*'
+t_DIVIDE = r'/'
+t_LP = r'\('
+t_RP = r'\)'
+t_COMMA = r','
+t_SEMICOLON = r';'
+
+t_NE = r'!=|<>'
+t_GE = r'>='
+t_LE = r'<='
+t_EQ = r'='
+t_GT = r'>'
+t_LT = r'<'
 
 
-class Lexer:
-    def __init__(self, sql: str):
-        self.sql = sql
-        self.position = 0
+def t_STR(t):
+    r'("[^"]*"|\'[^\']*\')'
+    return t
 
-    def _tokenize(self):
-        concrete_classes = get_concrete_classes(Token)
 
-        while self.position < len(self.sql):
-            match = None
-            for TokenClass in concrete_classes:
-                match = re.match(TokenClass.PATTERN, self.sql[self.position:])
-                if match:
-                    value = match.group(1)
-                    token = TokenClass(value)
-                    print(f'DEBUG LOG - Token: {token!r}')
-                    yield token
-                    self.position += match.end()
-                    break
+def t_SELECT(t):
+    r'SELECT'
+    return t
 
-            if not match:
-                self.position += 1
 
-    def get_tokens(self):
-        return list(self._tokenize())
+def t_INSERT(t):
+    r'INSERT|INSERT\s+INTO'
+    return t
+
+
+def t_UPDATE(t):
+    r'UPDATE'
+    return t
+
+
+def t_DELETE(t):
+    r'DELETE|DELETE\s+FROM'
+    return t
+
+
+def t_TRUNCATE(t):
+    r'TRUNCATE|TRUNCATE\s+TABLE'
+    return t
+
+
+def t_FROM(t):
+    r'FROM'
+    return t
+
+
+def t_WHERE(t):
+    r'WHERE'
+    return t
+
+
+def t_SET(t):
+    r'SET'
+    return t
+
+
+def t_AND(t):
+    r'AND|&'
+    return t
+
+
+def t_OR(t):
+    r'OR|\|'
+    return t
+
+
+def t_INT(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+
+def t_FLOAT(t):
+    r'(\d*\.\d+|\d+\.\d*)'
+    t.value = float(t.value)
+    return t
+
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    return t
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+t_ignore = ' \t'
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+lexer = lex.lex(reflags=re.IGNORECASE)
